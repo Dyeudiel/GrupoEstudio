362c971b0eaa8707b15e27843af5f24bf8babeec
Danielr76 <danielr2397@gamil.com>
2019-02-16 11:57:13 -0500

    task

--- src/main/java/com/toolsqa/tasks/Holamundo.java
+++ src/main/java/com/toolsqa/tasks/Holamundo.java
@@ -1,5 +1,15 @@
 package com.toolsqa.tasks;
 
-public class Holamundo {
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+
+public class Holamundo implements Task {
+
+	@Override
+	public <T extends Actor> void performAs(T actor) {
+
+		
+		
+	}
 
 }
--- src/test/java/com/toolsqa/runners/PracticeAutomationFormRunner.java
+++ src/test/java/com/toolsqa/runners/PracticeAutomationFormRunner.java
@@ -6,16 +6,9 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.SnippetType;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 
-
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/features/PracticeAutomationForm.feature",
-glue="com.toolsqa.steps",
-snippets=SnippetType.CAMELCASE)
-
+@CucumberOptions(features = "src/test/resources/features/PracticeAutomationForm.feature", glue = "com.toolsqa.steps", snippets = SnippetType.CAMELCASE)
 
 public class PracticeAutomationFormRunner {
 
-
-	
-	
 }
